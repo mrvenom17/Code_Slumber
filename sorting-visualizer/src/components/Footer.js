ac86dc6357a6762cc222ebcbdc430d588b247c82
Swati0702 <swatisinha.1537@gmail.com>
2025-02-28 15:21:46 +0530

    updating sorting-visualizer folder

--- sorting-visualizer/src/components/Footer.js
+++ sorting-visualizer/src/components/Footer.js
@@ -1,9 +1,11 @@
+import React from 'react';
+
 const Footer = () => {
-    return (
-        <footer className="text-center p-2 bg-dark text-white">
-            <p>&copy; {new Date().getFullYear()} Sorting Visualizer</p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p>&copy; 2025 Sorting Visualizer</p>
+    </footer>
+  );
 };
 
 export default Footer;
--- sorting-visualizer/src/components/Header.js
+++ sorting-visualizer/src/components/Header.js
@@ -1,14 +1,12 @@
-
-import React from "react";
-import logo from "../logo/logo.jpg";
+import React from 'react';
+import logo from './logo/logo.jpg';
 
 const Header = () => {
-    return (
-        <header className="text-center p-3 bg-dark text-white">
-            <img src={logo} alt="LazySort Logo" style={{ height: '50px' }} />
-            <h6>Sorting Visualizer</h6>
-        </header>
-    );
+  return (
+    <header>
+      <img src={logo} alt="LazySort Logo" style={{ height: '55px' }} />
+    </header>
+  );
 };
 
 export default Header;
